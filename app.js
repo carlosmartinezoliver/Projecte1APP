09ecae9bd80705593230f50ffedaa87bb1f085d8
Carlos <carlosmartinezoliver@gmail.com>
2015-05-20 19:57:52 +0200

    First commit

--- app.js
+++ app.js
@@ -206,8 +206,7 @@ app.controller('TodayCtrl', function($scope, $ionicModal, $ionicSlideBoxDelegate
 
         function onSuccess(imageData) {
             alert('OK! ' + imageData);
-            var image = document.getElementById('myImage');
-            $scope.image.src = "data:image/jpeg;base64," + imageData;
+            $scope.image = "data:image/jpeg;base64," + imageData;
         }
 
         function onFail(message) {
