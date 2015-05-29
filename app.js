233ce23cf8f5307331a1b243f62557806b6ee648
Carlos <carlosmartinezoliver@gmail.com>
2015-05-29 03:45:21 +0200

    x

--- app.js
+++ app.js
@@ -94,8 +94,8 @@ app.controller('GalleryCtrl', function($scope, $http, $ionicModal, $ionicActionS
     
     $scope.newPost = function() {
     	alert($scope.postId);
-    	alert($scope.post.title);
-    	alert($scope.post.content);
+    	alert($scope.title_post);
+    	alert($scope.content_post);
  	   
  	   $http.post("http://today.globals.cat/posts/" + $scope.postId + "/data/upload", 
  			   {title_post: $scope.title_post,
