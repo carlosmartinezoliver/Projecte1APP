0c397d11c0281a332e4d751f5bcaf6d8a64069d5
Carlos <carlosmartinezoliver@gmail.com>
2015-06-06 23:33:17 +0200

    x

--- app.js
+++ app.js
@@ -360,7 +360,7 @@ app.controller('NewPostCtrl', function($scope, $state, $http, $ionicActionSheet,
 	newId();
 	
 	function newId() {
-		$http.post("http://today.globals.cat/posts/create").
+		$http.post("http://today.globals.cat/posts/create", {}).
 	   	  success(function(data, status, headers, config) {
 	   		  alert("DONE!");
 	   		  alert(data);
