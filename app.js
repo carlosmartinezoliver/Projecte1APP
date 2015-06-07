fa2d35b2e8134e7892c277d54d6a1a48d4a98a0a
Carlos <carlosmartinezoliver@gmail.com>
2015-06-07 21:42:54 +0200

    x

--- app.js
+++ app.js
@@ -366,7 +366,7 @@ app.controller('NewPostCtrl', function($scope, $state, $http, $ionicActionSheet,
                                    // when the response is available
 
            $scope.postId = data.id;
-           alert($scope.postId);
+           alert(data.id);
         }).error(function(data, status, headers, config) {
                                    // called asynchronously if an error occurs
                                    // or server returns response with an error status.
